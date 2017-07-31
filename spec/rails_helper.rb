6c54f2e8c728c7421a1645b1310fb4ce6748dd44
b-i-hide <b.i1993patriots@gmail.com>
2017-07-31 15:27:59 +0900

    [add] rspecの設定を追加

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,5 +1,6 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
+require 'capybara/rspec'
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 # Prevent database truncation if the environment is production
@@ -56,4 +57,5 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+  config.include Capybara::DSL
 end
