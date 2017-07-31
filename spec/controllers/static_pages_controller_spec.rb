864a7e9a177ac57ca1271ce51c8f94eee8f8e8c2
b-i-hide <b.i1993patriots@gmail.com>
2017-07-31 12:54:41 +0900

    [add] static_controllerのテスト

---
+++ spec/controllers/static_pages_controller_spec.rb
@@ -0,0 +1,41 @@
+require 'rails_helper'
+
+RSpec.describe StaticPagesController, type: :controller do
+  describe 'GET #home' do
+    subject{ get :home }
+
+    it "responds successfully with an HTTP 200 status code" do
+      expect(subject).to be_success
+      expect(subject).to have_http_status(200)
+    end
+
+    it "renders the home template" do
+      expect(subject).to render_template('home')
+    end
+  end
+
+  describe 'GET #help' do
+    subject{ get :help }
+    it "responds successfully with an HTTP 200 status code" do
+      expect(subject).to be_success
+      expect(subject).to have_http_status(200)
+    end
+
+    it "renders the help template" do
+      expect(subject).to render_template('help')
+    end
+  end
+
+  describe 'GET #about' do
+    subject{ get :about }
+    it "responds successfully with an HTTP 200 status code" do
+      expect(subject).to be_success
+      expect(subject).to have_http_status(200)
+    end
+
+    it "renders the about template" do
+      get :about
+      expect(subject).to render_template ('about')
+    end
+  end
+end
