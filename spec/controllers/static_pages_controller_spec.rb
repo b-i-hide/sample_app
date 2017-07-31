9ffa9e744caf377d043eccf6543491c9a9561774
b-i-hide <b.i1993patriots@gmail.com>
2017-07-31 15:27:43 +0900

    [add] contactアクションの追加

--- spec/controllers/static_pages_controller_spec.rb
+++ spec/controllers/static_pages_controller_spec.rb
@@ -3,7 +3,6 @@ require 'rails_helper'
 RSpec.describe StaticPagesController, type: :controller do
   describe 'GET #home' do
     subject{ get :home }
-
     it "responds successfully with an HTTP 200 status code" do
       expect(subject).to be_success
       expect(subject).to have_http_status(200)
@@ -36,6 +35,19 @@ RSpec.describe StaticPagesController, type: :controller do
     it "renders the about template" do
       get :about
       expect(subject).to render_template ('about')
+    end
+  end
+
+  describe "GET #contact" do
+    subject{ get :contact }
+    it 'responds successfully with an HTTP 200 status code' do
+      expect(subject).to be_success
+      expect(subject).to have_http_status(200)
+    end
+
+    it "renders the about template" do
+      get :contact
+      expect(subject).to render_template ('contact')
     end
   end
 end
