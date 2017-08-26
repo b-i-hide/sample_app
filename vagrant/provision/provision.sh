d7ae7249c80e6d0ec21e8affd4dd29be44a498b8
b-i-hide <b.i1993patriots@gmail.com>
2017-08-26 14:23:53 +0900

    [fix] provisionの設定の修正

--- vagrant/provision/provision.sh
+++ vagrant/provision/provision.sh
@@ -8,7 +8,6 @@ ln -fs /develop/sample_app2 /var/www/
 
 yum -y update kernel
 yum -y install kernel-devel kernel-headers dkms gcc gcc-c++
-yum -y install ruby-build
 
 echo "Installing Git and setting it up..."
 yum -y install git
