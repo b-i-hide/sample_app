597de63aa87bc52bd2684a7c429752c2a74cf004
b-i-hide <b.i1993patriots@gmail.com>
2017-09-08 19:10:47 +0900

    [add] ユーザー登録の統合テスト

--- spec/features/users_signups_spec.rb
+++ spec/features/users_signups_spec.rb
@@ -7,7 +7,17 @@ RSpec.feature "UsersSignups", type: :feature do
     fill_in 'Email', with: 'foo@invalid'
     fill_in 'Password', with: 'foo'
     fill_in 'Confirmation', with: 'bar'
-    expect{ click_button 'Create my account' }.not_to change { User.count }
+    expect{ click_button 'Create my account' }.not_to change{ User.count }
     expect(page).to have_selector('h1', text: 'Sign up')
+  end
+
+  specify "vlaid signup infomation" do
+    visit signup_path
+    fill_in 'Name', with: 'test'
+    fill_in 'Email', with: 'test@example.com'
+    fill_in 'Password', with: '87654321'
+    fill_in 'Confirmation', with: '87654321'
+    expect{ click_button 'Create my account' }.to change{ User.count }.by(1)
+    expect(page).to have_selector('div.alert')
   end
 end
