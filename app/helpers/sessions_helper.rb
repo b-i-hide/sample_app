901470d5ad43e3e19977fd5df080c085b77324d5
b-i-hide <b.i1993patriots@gmail.com>
2017-10-23 17:28:47 +0900

    [add] ログアウト用のヘルパーメソッドを追加

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -11,4 +11,9 @@ module SessionsHelper
   def current_user
     @current_user ||= User.find_by(id: session[:user_id])
   end
+
+  def logout
+    session.delete(:user_id)
+    @current_user = nil
+  end
 end
