3fc3b36dedbc492c55226ee2f0e0b401b607d804
b-i-hide <b.i1993patriots@gmail.com>
2017-10-28 17:45:31 +0900

    [add] #updateの定義とbefore_actionの定義

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,11 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: %i(edit show update)
+
   def new
     @user = User.new
   end
 
   def show
-    @user = User.find(params[:id])
   end
 
   def create
@@ -19,12 +20,24 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find(params[:id])
+  end
+
+  def update
+    if @user.update_attributes(user_params)
+      flash[:success] = "Profile updated"
+      redirect_to user_path(@user)
+    else
+      render 'edit'
+    end
   end
 
   private
 
     def user_params
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
+
+    def set_user
+      @user = User.find(params[:id])
     end
 end
