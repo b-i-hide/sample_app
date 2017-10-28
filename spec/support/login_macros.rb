62e085c18e3a6c74868dee427fc9ac1f0a3deeca
b-i-hide <b.i1993patriots@gmail.com>
2017-10-28 17:46:35 +0900

    [add] users#editの統合テスト用のマクロを追加

--- spec/support/login_macros.rb
+++ spec/support/login_macros.rb
@@ -2,4 +2,24 @@ module LoginMacros
   def is_logged_in?
       !session[:user_id].nil?
   end
+
+  def log_in_as(user, options = {})
+    password = options[:password] || 'password'
+    remember_me = options[:remember_me] || '1'
+    if integration_test?
+      visit login_path
+      fill_in 'Email', with: user.email
+      fill_in 'Password', with: password
+      if remember_me == 1
+        check 'Remember me on this computer'
+      end
+      click_button 'Log in'
+    else
+      session[:user_id] = user.id
+    end
+  end
+
+  def integration_test?
+    !respond_to?(:session)
+  end
 end
